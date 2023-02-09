2b36efbcdd0151766ca243f6a5e46c42ef14c8b2
aborn <loveaborn@foxmail.com>
2023-02-09 17:45:04 +0800

    feat: add java serve code.

---
+++ codepulse-serv/src/main/java/com/github/aborn/codepulse/codepulseserv/CodepulseServApplication.java
@@ -0,0 +1,13 @@
+package com.github.aborn.codepulse.codepulseserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepulseServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodepulseServApplication.class, args);
+	}
+
+}
---
+++ codepulse-serv/src/test/java/com/github/aborn/codepulse/codepulseserv/CodepulseServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aborn.codepulse.codepulseserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodepulseServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
