e293258a29bd311aa1d465f10f8725545e46faa5
aborn <loveaborn@foxmail.com>
2023-02-13 15:39:08 +0800

    feat: week trend data show.

---
+++ codepulse-serv/src/main/java/com/github/aborn/codepulse/admin/datatypes/WeekDayItem.java
@@ -0,0 +1,47 @@
+package com.github.aborn.codepulse.admin.datatypes;
+
+import com.github.aborn.codepulse.common.datatypes.DayBitSet;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author aborn (jiangguobao)
+ * @date 2023/02/13 14:50
+ */
+@Data
+@NoArgsConstructor
+public class WeekDayItem {
+    public static final Map<Integer, String> WEEK_DAY = new HashMap<>();
+    static {
+        WEEK_DAY.put(0, "一");
+        WEEK_DAY.put(1, "二");
+        WEEK_DAY.put(2, "三");
+        WEEK_DAY.put(3, "四");
+        WEEK_DAY.put(4, "五");
+        WEEK_DAY.put(5, "六");
+        WEEK_DAY.put(6, "日");
+    }
+    private String day;
+
+    private int dayOfWeek;
+
+    private String title;
+    private UserActionResponse action;
+
+    public WeekDayItem(String day, int dayOfWeek) {
+        this.day = day;
+        this.dayOfWeek = dayOfWeek;
+        this.title = day + "（周" + WEEK_DAY.get(dayOfWeek) + "）";
+    }
+
+    public void setDefaultDayBitSet(String token) {
+        this.action =  new UserActionResponse(new DayBitSet(this.day, token));
+    }
+
+    public void setAction(DayBitSet data) {
+        this.action = new UserActionResponse(data);
+    }
+}
