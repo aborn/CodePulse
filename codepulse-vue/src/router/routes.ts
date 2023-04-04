7001e58a212beb7776a6cbc8f740eb40d56cda4f
aborn <loveaborn@foxmail.com>
2023-04-04 11:03:36 +0800

    feat: remove d.ts

--- codepulse-vue/src/router/routes.ts
+++
@@ -1,9 +0,0 @@
-import { EXCEPTION_COMPONENT, PAGE_NOT_FOUND_NAME, E404_COMPONENT } from '/@/router/constant';
-
-export const basicRouteItems = [
-    { path: '/', name: 'Home', component: () => import('/@/pages/index.vue') },       
-    { path: '/demo', name: 'Demo', component: () => import('/@/pages/demo.vue') },
-    { path: '/index', name: 'Index', component: () => import('/@/pages/index.vue') },
-    { path: '/404', name: '404Page', component: E404_COMPONENT },
-    { path: '/:path(.*)*', name: PAGE_NOT_FOUND_NAME, component: EXCEPTION_COMPONENT, }
-]
