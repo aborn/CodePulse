50b2b4cf8e1326795f76f5744b8416268134e7bd
aborn <loveaborn@foxmail.com>
2023-04-04 10:29:25 +0800

    feat: 重新构建脚本

---
+++ codepulse-serv/scripts/rebuild.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+cd $DIR
+cd ..
+
+# 打jar包
+mvn package
+
+# 将jar包copy到指定目录
+cp ./target/codepulse-serv-0.0.1-SNAPSHOT.jar /Users/aborn/docker/packages
+
+# 重启动容器，让jar包生效
+docker restart codepulse_serv
