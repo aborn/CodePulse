4d9f5ab3f54e3d1b6fe8adfeb390cea65b030160
aborn <loveaborn@foxmail.com>
2024-05-07 19:15:03 +0800

    配置文件地址修改

---
+++ plugins/codepulse-visualstudio/codepulse-visualstudio/CodePulse/CodePulseConsts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodePulse
+{
+    public class CodePulseConsts
+    {
+        public static string CODE_PULSE = "CodePulse";
+        public static string ROOT_PATH = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), CODE_PULSE);
+        public static string CONFIG_FILE = Path.Combine(ROOT_PATH,  CODE_PULSE.ToLower() + ".cfg");
+        public static string LOG_FILE = Path.Combine(ROOT_PATH, CODE_PULSE.ToLower() + ".log");
+    }
+}
